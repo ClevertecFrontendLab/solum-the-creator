ac7f7bf9be685ada8676fe556e11de4f84f958be
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-04-04 15:32:56 +0300

    feat(theme): add custom theme and ChakraProvider

---
+++ src/constants/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+    colors: {
+        lime: {
+            50: '#ffffd3',
+            100: '#eaffc7',
+            200: '#d7ff94',
+            300: '#c4ff61',
+            400: '#bdff3d',
+            500: '#b1ff2e',
+            600: '#2db100',
+            700: '#207e00',
+            800: '#134b00',
+            900: '#003900',
+        },
+    },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -7,10 +8,14 @@ import { Provider } from 'react-redux';
 import App from '~/app/App.tsx';
 import { store } from '~/store/configure-store.ts';
 
+import { theme } from './constants/theme';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <ChakraProvider theme={theme}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </ChakraProvider>
     </StrictMode>,
 );
