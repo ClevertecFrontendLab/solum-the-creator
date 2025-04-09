8411c9ae47ac5650fa63b19d82482d3351c9b861
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-04-09 13:26:20 +0300

    feat(logo): add link to Home page

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,7 +1,9 @@
-import { chakra, Flex } from '@chakra-ui/react';
+import { Box, chakra, Flex } from '@chakra-ui/react';
+import { Link } from 'react-router';
 
 import LogoIcon from '~/assets/icons/logo-icon.svg?react';
 import LogoTextIcon from '~/assets/icons/logo-text.svg?react';
+import { pathes } from '~/constants/pathes';
 import { getDisplay } from '~/utils/get-display-logo';
 
 type LogoProps = {
@@ -15,9 +17,13 @@ export const Logo: React.FC<LogoProps> = ({ isFull = false, responsive = true })
     const display = getDisplay(isFull, responsive);
 
     return (
-        <Flex alignItems='flex-end' gap={1}>
-            <LogoIcon />
-            <LogoTextIconChakra display={display} />
-        </Flex>
+        <Box>
+            <Link to={pathes.home}>
+                <Flex alignItems='flex-end' gap={1}>
+                    <LogoIcon />
+                    <LogoTextIconChakra display={display} />
+                </Flex>
+            </Link>
+        </Box>
     );
 };
