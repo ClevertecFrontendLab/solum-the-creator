0d6e2ce3c3fad99c95ae4a988777e0d3f4c9ecbe
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-04-09 23:24:25 +0300

    feat(header): create BurgerButton component

---
+++ src/components/burger-button/burger-button.tsx
@@ -0,0 +1,20 @@
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { IconButton } from '@chakra-ui/react';
+
+type BurgerButtonProps = {
+    isOpen?: boolean;
+    onOpen?: () => void;
+    onClose?: () => void;
+};
+
+export const BurgerButton: React.FC<BurgerButtonProps> = ({ isOpen, onOpen, onClose }) => (
+    <IconButton
+        size='md'
+        mx={1}
+        icon={isOpen ? <CloseIcon boxSize={3} /> : <HamburgerIcon boxSize={5} />}
+        aria-label='Toggle menu'
+        display={{ md: 'none', base: 'flex' }}
+        onClick={isOpen ? onClose : onOpen}
+        variant='clear'
+    />
+);
