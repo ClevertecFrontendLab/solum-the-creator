2dac0cf92670b94adaaa682c80c9eac4b174fe68
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-04-11 14:18:30 +0300

    feat(sidebar): create sidebar footer component

--- src/components/layouts/sidebar/sidebar-footer.tsx
+++ src/components/layouts/sidebar/sidebar-footer.tsx
@@ -1,7 +1,20 @@
-import { Box } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+
+import ExitIcon from '~/assets/icons/exit-icon.svg?react';
 
 export const SidebarFooter: React.FC = () => (
-    <>
-        <Box>footer</Box>
-    </>
+    <Flex as='footer' gap={4} pb={8} px={6} pt={2} direction='column'>
+        <Text as='span' fontSize='xs' color='blackAlpha.400' fontWeight='500'>
+            Версия программы 03.25
+        </Text>
+        <Text fontSize='xs' color='blackAlpha.700' fontWeight='400'>
+            Все права защищены, ученический файл, <br />
+            ©Клевер Технолоджи, 2025
+        </Text>
+        <Flex>
+            <Button size='xs' leftIcon={<ExitIcon />} colorScheme='black' variant='ghost' px={0}>
+                Выйти
+            </Button>
+        </Flex>
+    </Flex>
 );
