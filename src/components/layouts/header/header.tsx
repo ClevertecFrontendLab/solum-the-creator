a0abd8334faa60ba47b92ac5ff6205e87c5dbc48
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-04-15 16:13:20 +0300

    refactor(components): remove unnecessary wrappers and improve readability

--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Spacer, useDisclosure } from '@chakra-ui/react';
+import { Box, Flex, Hide, Show, Spacer, useDisclosure } from '@chakra-ui/react';
 
 import avatarUrl from '~/assets/images/avatar.jpg';
 import { ProfileNotification } from '~/components/entities/profile-notification/profile-notification';
@@ -14,7 +14,7 @@ export const Header: React.FC = () => {
         <Box
             as='header'
             bg='lime.50'
-            px={{ base: 4, md: 4, sm: 5 }}
+            px={{ base: 4, sm: 5 }}
             py={4}
             top={0}
             width='100%'
@@ -23,30 +23,28 @@ export const Header: React.FC = () => {
             <Flex h={{ base: 8, md: 12 }} alignItems='center' width='100%'>
                 <Logo isFull={true} />
 
-                <Spacer maxW={{ base: 0, md: 32 }} />
-
-                <Box display={{ base: 'none', md: 'block' }}>
+                <Show above='md'>
                     <Breadcrumbs />
-                </Box>
+                </Show>
 
                 <Spacer />
 
-                <Box display={{ base: 'none', md: 'block' }} pr={{ base: 0, lg: 6, xl: 16 }}>
-                    <UserInfoCard
-                        avatarUrl={avatarUrl}
-                        fullName='Екатерина Константинопольская'
-                        userName='bake_and_pie'
-                    />
-                </Box>
+                <Show above='md'>
+                    <Box pr={{ base: 0, lg: 6, xl: 16 }}>
+                        <UserInfoCard
+                            avatarUrl={avatarUrl}
+                            fullName='Екатерина Константинопольская'
+                            userName='bake_and_pie'
+                        />
+                    </Box>
+                </Show>
 
-                <Flex
-                    display={{ base: 'flex', md: 'none' }}
-                    align='center'
-                    gap={{ base: 2, sm: 4 }}
-                >
-                    <ProfileNotification values={[121, 23, 49]} />
-                    <BurgerButton isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
-                </Flex>
+                <Hide above='md'>
+                    <Flex align='center' gap={{ base: 2, sm: 4 }}>
+                        <ProfileNotification values={[121, 23, 49]} />
+                        <BurgerButton isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+                    </Flex>
+                </Hide>
             </Flex>
         </Box>
     );
