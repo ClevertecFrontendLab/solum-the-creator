75627a6a8cbcc9395b62adcce3ae2840c4ca32d6
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-04-22 11:43:50 +0300

    feat(navigation): add filtering by recipe categories and subcategories

--- src/pages/category/subcategory/subcategory-page.tsx
+++ src/pages/category/subcategory/subcategory-page.tsx
@@ -1,4 +1,9 @@
 import { RecipeHorizontalGridSection } from '~/components/widgets/recipe-horizontal-grid-section/recipe-horizontal-grid-section';
 import { recipes } from '~/constants/data/recipes';
+import { useFilteredRecipes } from '~/hooks/use-filtered-recipes';
 
-export const SubcategoryPage = () => <RecipeHorizontalGridSection recipes={recipes} />;
+export const SubcategoryPage = () => {
+    const filteredRecipes = useFilteredRecipes(recipes);
+
+    return <RecipeHorizontalGridSection recipes={filteredRecipes} />;
+};
