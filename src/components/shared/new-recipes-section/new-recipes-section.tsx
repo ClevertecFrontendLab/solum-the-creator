dc35d0b0dbbe6e50e2c7c4cba4382a81bcc5ad33
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-04-25 14:40:20 +0300

    feat(slider): add sorting by date of adding recipes

--- src/components/shared/new-recipes-section/new-recipes-section.tsx
+++ src/components/shared/new-recipes-section/new-recipes-section.tsx
@@ -4,31 +4,36 @@ import { RecipeCardVertical } from '~/components/shared/cards/recipe-card/recipe
 import { FullBleed } from '~/components/shared/full-bleed/full-bleed';
 import { HorizontalSlider } from '~/components/ui/horizontal-slider/horizontal-slider';
 import { recipes } from '~/constants/data/recipes';
+import { getNewestRecipes } from '~/utils/sort';
 
-export const NewRecipesSection = () => (
-    <Box as='section' position='relative' width='100%'>
-        <Heading mb={{ base: 3, md: 6 }} variant='section-title'>
-            Новые рецепты
-        </Heading>
+export const NewRecipesSection = () => {
+    const newRecipes = getNewestRecipes(recipes);
 
-        <FullBleed>
-            <HorizontalSlider
-                items={recipes}
-                renderItem={(recipe) => (
-                    <RecipeCardVertical
-                        key={recipe.id}
-                        id={recipe.id}
-                        title={recipe.title}
-                        description={recipe.description}
-                        image={recipe.image}
-                        category={recipe.category}
-                        subcategory={recipe.subcategory}
-                        likes={recipe.likes}
-                        bookmarks={recipe.bookmarks}
-                        forceFromRecipe={true}
-                    />
-                )}
-            />
-        </FullBleed>
-    </Box>
-);
+    return (
+        <Box as='section' position='relative' width='100%'>
+            <Heading mb={{ base: 3, md: 6 }} variant='section-title'>
+                Новые рецепты
+            </Heading>
+
+            <FullBleed>
+                <HorizontalSlider
+                    items={newRecipes}
+                    renderItem={(recipe) => (
+                        <RecipeCardVertical
+                            key={recipe.id}
+                            id={recipe.id}
+                            title={recipe.title}
+                            description={recipe.description}
+                            image={recipe.image}
+                            category={recipe.category}
+                            subcategory={recipe.subcategory}
+                            likes={recipe.likes}
+                            bookmarks={recipe.bookmarks}
+                            forceFromRecipe={true}
+                        />
+                    )}
+                />
+            </FullBleed>
+        </Box>
+    );
+};
---
+++ src/utils/sort.ts
@@ -0,0 +1,6 @@
+import { Recipe } from '~/constants/data/recipes';
+
+export const getNewestRecipes = (recipes: Recipe[], count = 10) =>
+    [...recipes]
+        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+        .slice(0, count);
