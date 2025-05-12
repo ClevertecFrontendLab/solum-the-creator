00eb6efbbbecacd51d193b411cb48cde754d9af5
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-05-12 21:51:00 +0300

    feat(auth): create LoginForm layout

--- src/pages/auth/login-page.tsx
+++ src/pages/auth/login-page.tsx
@@ -1,14 +1,31 @@
+import { Button, Text, VStack } from '@chakra-ui/react';
+import { Link } from 'react-router';
+
 import { FormInput } from '~/components/shared/inputs/form-input/form-input';
 
 export const LoginPage = () => (
-    <div>
-        <h1>login page</h1>
-        <FormInput
-            label='Логин для входа на сайт'
-            type='password'
-            helperText='Введите пароль'
-            placeholder='Пароль'
-            showPasswordToggle={true}
-        />
-    </div>
+    <VStack as='form' w='100%'>
+        <VStack w='100%' spacing={6}>
+            <FormInput label='Логин для входа на сайт' type='text' placeholder='Введите логин' />
+
+            <FormInput
+                label='Пароль'
+                type='password'
+                placeholder='Пароль для сайта'
+                showPasswordToggle={true}
+            />
+        </VStack>
+
+        <VStack w='100%' spacing={4} mt='7rem'>
+            <Button type='submit' variant='black' w='100%' size='lg'>
+                Войти
+            </Button>
+
+            <Link to='#'>
+                <Text as='span' fontWeight={600}>
+                    Забыли логин или пароль?
+                </Text>
+            </Link>
+        </VStack>
+    </VStack>
 );
