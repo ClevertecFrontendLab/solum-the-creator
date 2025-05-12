b174c456cbed608e1933e62ffe551f3c0c70e18a
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2025-05-12 17:55:09 +0300

    feat(auth): create AuthLayout component

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -1,11 +1,103 @@
-import { Box, Flex, HStack } from '@chakra-ui/react';
-import { Outlet } from 'react-router';
+import {
+    Box,
+    Grid,
+    GridItem,
+    Image,
+    Tab,
+    TabList,
+    Tabs,
+    Text,
+    useBreakpointValue,
+    VStack,
+} from '@chakra-ui/react';
+import { Link, Outlet } from 'react-router';
 
-export const AuthLayout = () => (
-    <HStack>
-        <Flex>
-            <Outlet />
-        </Flex>
-        <Box>Image</Box>
-    </HStack>
-);
+import authImage from '~/assets/images/auth-image.jpg';
+import { pathes } from '~/constants/navigation/pathes';
+
+import { Logo, LogoSize } from '../shared/misc/logo/logo';
+
+export const AuthLayout = () => {
+    const showImage = useBreakpointValue({ base: false, md: true });
+    const logoSize = useBreakpointValue<LogoSize>({ base: 'md', md: 'lg' });
+
+    return (
+        <Grid
+            templateColumns={{
+                base: '1fr',
+                md: 'minmax(30rem, 49%) minmax(0, 1fr)',
+            }}
+            minH='100vh'
+            w='100%'
+            bgGradient='linear(230deg, #eafec7 0%, #eafec7 29.89%, #75b374 100%)'
+        >
+            <GridItem
+                display='flex'
+                flexDirection='column'
+                alignItems='start'
+                justifyContent='space-between'
+                px={{ base: 4, sm: 5 }}
+                pt={{ base: '4.5rem', sm: '8.75rem', md: '10.625rem' }}
+                pb={{ base: 4, sm: 5 }}
+            >
+                <VStack
+                    alignSelf='center'
+                    spacing={{ base: '2.5rem', sm: '3.5rem', md: '5rem' }}
+                    w='100%'
+                    maxW={{ base: '100%', sm: '22.25rem', md: '28.25rem', '2xl': '28.875rem' }}
+                >
+                    <Logo isFull={true} responsive={false} size={logoSize} to={pathes.login} />
+                    <VStack w='100%' spacing={10}>
+                        <Tabs w='100%' variant='auth'>
+                            <TabList>
+                                <Tab as={Link} to={pathes.login}>
+                                    Вход на сайт
+                                </Tab>
+
+                                <Tab as={Link} to={pathes.signUp}>
+                                    <Box>Регистрация</Box>
+                                </Tab>
+                            </TabList>
+                        </Tabs>
+                        <Outlet />
+                    </VStack>
+                </VStack>
+                <Box as='footer' p={2.5}>
+                    <Text fontSize='sm' fontWeight={600}>
+                        Все права защищены, ученический файл, ©Клевер Технолоджи, 2025
+                    </Text>
+                </Box>
+            </GridItem>
+
+            {showImage && (
+                <GridItem
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
+                    minW={0}
+                    h='100vh'
+                >
+                    <Image
+                        src={authImage}
+                        alt='Food on table'
+                        objectFit='cover'
+                        w='100%'
+                        h='100%'
+                    />
+
+                    <Text
+                        position='absolute'
+                        bottom={5}
+                        right={5}
+                        p={2.5}
+                        fontSize='sm'
+                        fontWeight={600}
+                        zIndex={1}
+                    >
+                        ̶ Лучший сервис для ваших кулинарных побед
+                    </Text>
+                </GridItem>
+            )}
+        </Grid>
+    );
+};
--- src/components/shared/misc/logo/logo.tsx
+++ src/components/shared/misc/logo/logo.tsx
@@ -1,4 +1,4 @@
-import { Box, chakra, Flex } from '@chakra-ui/react';
+import { Box, chakra, Flex, Icon } from '@chakra-ui/react';
 import { Link } from 'react-router';
 
 import LogoIcon from '~/assets/icons/logo-icon.svg?react';
@@ -6,23 +6,44 @@ import LogoTextIcon from '~/assets/icons/logo-text.svg?react';
 import { pathes } from '~/constants/navigation/pathes';
 import { getDisplay } from '~/utils/get-display-logo';
 
+export type LogoSize = 'sm' | 'md' | 'lg';
+
 type LogoProps = {
+    to?: string;
     isFull?: boolean;
+    size?: LogoSize;
     responsive?: boolean;
     onClick?: () => void;
 };
 
 const LogoTextIconChakra = chakra(LogoTextIcon);
 
-export const Logo: React.FC<LogoProps> = ({ onClick, isFull = false, responsive = true }) => {
+export const Logo: React.FC<LogoProps> = ({
+    onClick,
+    to = pathes.home,
+    size = 'sm',
+    isFull = false,
+    responsive = true,
+}) => {
     const display = getDisplay(isFull, responsive);
+
+    const logoBoxSize = { sm: '32px', md: '38px', lg: '64px' };
+    const textBoxSize = {
+        width: { sm: '6rem', md: '7rem', lg: '12rem' },
+        height: { sm: '1.5rem', md: '1.875rem', lg: '3.125rem' },
+    };
 
     return (
         <Box>
-            <Link to={pathes.home} onClick={onClick}>
-                <Flex alignItems='center' gap={1}>
-                    <LogoIcon />
-                    <LogoTextIconChakra display={display} />
+            <Link to={to} onClick={onClick}>
+                <Flex alignItems='end' gap={1}>
+                    <Icon as={LogoIcon} boxSize={logoBoxSize[size]} />
+                    <Icon
+                        as={LogoTextIconChakra}
+                        display={display}
+                        w={textBoxSize.width[size]}
+                        h={textBoxSize.height[size]}
+                    />
                 </Flex>
             </Link>
         </Box>
